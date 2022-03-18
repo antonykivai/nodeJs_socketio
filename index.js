d703ff0a2a4897755e12e8b391c574ffd9c62a2e
antony <antony@localhost>
2022-03-18 12:59:54 +0300

    fix: Fixed bug double counting occurrences of a value
    Rewrote findOccurrence function to fix the problem of an occurrence
    being double counted

--- index.js
+++ index.js
@@ -6,28 +6,21 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-function findOcc(arr, key){
-    let arr2 = [];
-      
-    arr.forEach((x)=>{
-       if(arr2.some((val)=>{ return val[key] == x[key] })){
-         arr2.forEach((k)=>{
-           if(k[key] === x[key]){ 
-             k["occurrence"]++
-           }
-        })
-       }else{
-         let a = {}
-         a[key] = x[key]
-         a["occurrence"] = 1
-         arr2.push(a);
-       }
-    })
-    return arr2
-  }
+function findOcc(arr) {
+    const valueOccurrences = {};
+
+    for (const element of arr) {
+        if (valueOccurrences[element]) {
+            valueOccurrences[element] += 1;
+        } else {
+            valueOccurrences[element] = 1;
+        }
+    }
+    return valueOccurrences
+}
 
 let valuesList = [];
 
@@ -37,18 +30,14 @@ io.on('connection', (socket) => {
     //Consuming events
     io.on('connection', (socket) => {
         socket.on('chat_message', (msg) => {
-            //io.emit('chat_message', msg);
-          //console.log('message: ' + msg);
-          valuesList.push({"value" : msg})
-          console.log(findOcc(valuesList, "value"));
-          io.emit('chat_message', findOcc(valuesList, "value"))
-          //io.emit('chat_message', msg)
+            valuesList.push(msg)
+            io.emit('chat_message', findOcc(valuesList, "value"))
         });
-      });
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
     });
-  });
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+server.listen(3020, () => {
+    console.log('listening on *:3020');
 });
